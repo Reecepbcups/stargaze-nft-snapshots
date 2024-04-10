2540ba9db7836f3550bf46201e4f16de2bffd4bc
Reece Williams <reecepbcups@gmail.com>
2024-04-09 20:58:17 -0500

    iter all projects and codence outpout

---
+++ one-off-condence.py
@@ -0,0 +1,51 @@
+import asyncio
+import datetime
+import json
+import os
+import random
+
+from helpers import CompactJSONEncoder
+
+PROJECTS = [
+    "badkids",
+    "bitkids",
+    "celestine_sloth_society",
+    "racoon",
+    "cryptoniummaker",
+    "afterthefilter",
+    "pixelsquids",
+    "sneaky",
+    "geckies",
+    "rektbulls",
+]
+
+
+def main():
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+
+    for project in PROJECTS:
+
+        directory = os.path.join(curr_dir, f"{project}")
+        if not os.path.exists(directory):
+            exit(f"directory {directory} does not exist")
+
+        files = os.listdir(directory)
+
+        for file in files:
+
+            fp = os.path.join(directory, file)
+
+            with open(fp, "r") as f:
+                data = json.load(f)
+
+            print(f"Processing {fp}...")
+
+            # save data back to file with the cls CompactJSONEncoder
+            with open(fp, "w") as f:
+                json.dump(data, f, cls=CompactJSONEncoder, indent=4)
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
